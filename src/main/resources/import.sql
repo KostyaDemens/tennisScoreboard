abc6ba78790bb1c3239a68b1a8033f19c576ed82
KostyaDemens <kostyademens@gmail.com>
2024-05-21 21:29:52 +0300

    Fixed import.sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,30 +1,14 @@
-INSERT INTO players (name)
-VALUES ('IVAN'),
-       ('ILIA'),
-       ('KIRILL'),
-       ('BOB'),
-       ('ANDREW'),
-       ('LIZA');
+INSERT INTO players (name) VALUES ('IVAN'), ('ILIA'), ('KIRILL'), ('BOB'), ('ANDREW'), ('LIZA');
 
 
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (1, 2, 1);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (3, 4, 3);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (5, 6, 6);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (4, 2, 4);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (3, 1, 1);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (5, 2, 5);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (6, 4, 4);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (3, 2, 2);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (5, 2, 2);
-INSERT INTO matches(player_1, player_2, winner)
-VALUES (2, 1, 2);
+INSERT INTO matches(player_1, player_2, winner) VALUES (1, 2, 1);
+INSERT INTO matches(player_1, player_2, winner) VALUES (3, 4, 3);
+INSERT INTO matches(player_1, player_2, winner) VALUES (5, 6, 6);
+INSERT INTO matches(player_1, player_2, winner) VALUES (4, 2, 4);
+INSERT INTO matches(player_1, player_2, winner) VALUES (3, 1, 1);
+INSERT INTO matches(player_1, player_2, winner) VALUES (5, 2, 5);
+INSERT INTO matches(player_1, player_2, winner) VALUES (6, 4, 4);
+INSERT INTO matches(player_1, player_2, winner) VALUES (3, 2, 2);
+INSERT INTO matches(player_1, player_2, winner) VALUES (5, 2, 2);
+INSERT INTO matches(player_1, player_2, winner) VALUES (2, 1, 2);
 
